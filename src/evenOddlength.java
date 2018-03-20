1b911f4b75bacf3467562c2487c706be94f89fdb
Maurice Hanns, Jr <mhanns@yahoo.com>
2018-03-20 12:13:00 -0400

    Update evenOddlength.java

--- src/evenOddlength.java
+++ src/evenOddlength.java
@@ -1,9 +1,41 @@
+//Maurice Hanns CSC 242
 
+import java.util.Scanner;
+	
 public class evenOddlength {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
 
-}
+					
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter the string: ");
+			
+			String string1 = input.nextLine();
+			boolean what = even(string1);
+			System.out.println("Is the number even: " + what);
+
+								
+		}//main
+
+		public static boolean even(String strg){
+			
+			boolean val = false;	
+			int length = strg.length();
+		
+			System.out.println(length);
+			System.out.println(strg);
+
+		
+			if (length % 2 == 0) {
+				val = true;
+			}
+			else if (length % 2 != 0){
+				val = false;
+			}
+		
+			return val;
+		}//even
+		
+	}//class
